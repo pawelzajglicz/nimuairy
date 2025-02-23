3ea484010de1d3a6cd9617a9741fe150f4c63066
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2025-02-23 14:24:47 +0100

    Init backend

---
+++ nimuairy-api/src/main/java/com/nimuairy/nimuairy/NimuairyApiApplication.java
@@ -0,0 +1,13 @@
+package com.nimuairy.nimuairy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimuairyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimuairyApiApplication.class, args);
+	}
+
+}
---
+++ nimuairy-api/src/test/java/com/nimuairy/nimuairy/NimuairyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimuairy.nimuairy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimuairyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
